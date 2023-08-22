a51768f411906f698c21e70c1c728d022c1b94e3
kikiandre5 <38192660@uaz.edu.mx>
2023-08-22 12:52:58 -0600

    Actividad 1
    Primer Actividad

---
+++ p01-hola-mundo/Program.cs
@@ -0,0 +1,10 @@
+﻿// Programa que manda saludos a la pantalla
+
+string amigo = "Adrian";
+string hermano = "Paco";
+
+Console.WriteLine("Hola " + amigo);
+Console.WriteLine($"Hola {hermano} bienvenido a c#");
+Console.WriteLine($"\nMi amigo es {amigo} mi hermano es {hermano}");
+Console.WriteLine("Nombres en mayusculas");
+Console.WriteLine($"{amigo.ToUpper()} {hermano.ToUpper()}");
---
+++ p02-paga-trabajador/Program.cs
@@ -0,0 +1,30 @@
+﻿// Calcula la paga de un trabajador, considerando las horas extras
+
+string nombre="";
+int horas=0,hextra=0;
+float paga=0, pagaextra=0, pagabruta=0, paganeta=0, impuesto=0, tasa=0;
+
+Console.Clear();
+Console.Write("Cual es tu nombre? "); nombre = Console.ReadLine();
+Console.Write("Horas "); horas = int.Parse(Console.ReadLine());
+Console.Write("Paga "); paga = float.Parse(Console.ReadLine());
+
+tasa = 0.03f;
+
+if(horas > 40 ){
+    hextra = horas - 40;
+    pagaextra = 2*paga*hextra;
+    pagabruta = 40*paga+pagaextra;
+}
+else
+    pagabruta = horas*paga;
+
+impuesto = pagabruta * tasa;
+paganeta = pagabruta - impuesto;
+
+Console.WriteLine($"\nResumen de pagos del trabajador {nombre} \n");
+Console.WriteLine($"{horas} Horas Trabajada, {hextra} horas extra, paga {paga}, tasa de impuesto {tasa:p2}");
+Console.WriteLine($"Paga Bruta: {pagabruta,12:c2}");
+Console.WriteLine($"Paga Extra: {pagaextra,12:c2}");
+Console.WriteLine($"Impuesto: {impuesto,12:c2}");
+Console.WriteLine($"Paga Neta: {paganeta,12:c2}");
